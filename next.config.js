14901c25c1f5ec213469bc27d71bcdcf1ac0f1ee
pr-o <sunghahh.git@gmail.com>
2022-08-21 20:47:06 +0900

    from spaces to tabs

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 module.exports = (phase, { defaultConfig }) => {
-	const webpackConfig = {
-		webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-			config.module.rules.push({
-				test: /\.(glsl)$/,
-				use: ['glslify-import-loader', 'raw-loader', 'glslify-loader'],
-			});
+  const webpackConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.module.rules.push({
+        test: /\.(glsl)$/,
+        use: ['glslify-import-loader', 'raw-loader', 'glslify-loader'],
+      });
 
-			return config;
-		},
-	};
+      return config;
+    },
+  };
 
-	const nextConfig = {
-		reactStrictMode: true,
-		swcMinify: true,
-	};
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  };
 
-	if (phase === PHASE_DEVELOPMENT_SERVER) {
-		return {
-			...webpackConfig,
-			...nextConfig,
-		};
-	}
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...webpackConfig,
+      ...nextConfig,
+    };
+  }
 
-	return {
-		...webpackConfig,
-		...nextConfig,
-	};
+  return {
+    ...webpackConfig,
+    ...nextConfig,
+  };
 };
